09833cf4d9356cecb21464accc60905b16b92252
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2025-01-15 21:31:50 +0700

    add week1

---
+++ week1/hellojs.js
@@ -0,0 +1,4 @@
+let a; //changeable
+const b = 2; //constant
+
+console.log(a);
---
+++ week1/type.ts
@@ -0,0 +1,9 @@
+const hi:string = 'Hello World';
+console.log(hi);
+
+function getMessage(msg: string): string {
+    return msg;
+}
+
+let msg: string = 'Hello World';
+console.log(getMessage(msg));
