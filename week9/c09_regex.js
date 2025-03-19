8f3239944c7ed26d46f34fcb1e1fa0d3a4efb5d7
Taiyo Yamamoto <120293524+Ex0TiiC24@users.noreply.github.com>
2025-03-19 10:51:22 +0700

    add week09 regex

---
+++ week9/c09_regex.js
@@ -0,0 +1,18 @@
+//regex.js
+function p(...a){
+    console.log(...a)
+}
+
+function createRegex(){
+    
+    let legeg = /\w{6,}@[a-z]+.com/g
+    let pass = /\w{8,}/i
+    let number = /\d+/g
+    // p(legeg.test('taiyoyama@outlook.com'))
+    // p(pass.test('hwefiwefji@9234923-+-_-wdw[}}'))
+    p("daowmdoamwdojfiwje.miscellaneouse@gmail.com".search(legeg))
+    p("daowmdoamwdojfiwje.miscellaneouse@gmail.com.omegalul@gmail.com".match(legeg))
+    p("miscellaneo123use22@gmail.com".match(number))
+}
+
+createRegex()
