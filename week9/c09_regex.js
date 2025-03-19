fe8bbfa0f368a702e426d0ac02c9c06c5e6a1d75
Taiyo Yamamoto <120293524+Ex0TiiC24@users.noreply.github.com>
2025-03-19 11:05:53 +0700

    update week09

--- week9/c09_regex.js
+++ week9/c09_regex.js
@@ -5,14 +5,17 @@ function p(...a){
 
 function createRegex(){
     
-    let legeg = /\w{6,}@[a-z]+.com/g
+    let legeg = /\w{6,}@[a-z]+(.com)/g
     let pass = /\w{8,}/i
     let number = /\d+/g
     // p(legeg.test('taiyoyama@outlook.com'))
     // p(pass.test('hwefiwefji@9234923-+-_-wdw[}}'))
     p("daowmdoamwdojfiwje.miscellaneouse@gmail.com".search(legeg))
     p("daowmdoamwdojfiwje.miscellaneouse@gmail.com.omegalul@gmail.com".match(legeg))
+
+    for(let o of "miscellaneouse@gmail.com.omegalul@gmail.com".matchAll(legeg)) p(o)
     p("miscellaneo123use22@gmail.com".match(number))
+
 }
 
 createRegex()
